c1f8afbffe79a7c7c9343a494a135bcff3aaf759
Matt Wilkins <mattwilkins@MacBook-Pro.local>
2018-12-14 17:23:51 -0600

    Switched controls & table position; added box around plot

--- app.R
+++ app.R
@@ -18,23 +18,28 @@ ui <- fluidPage(
    # Application title
    titlePanel("The Jobverse: Explore the Possibilities"),
    
+   tableOutput("tab"),
+  
+  br(),
+      # Show a plot of the generated distribution
+     fluidRow(
+       column(1),
+       column(style = "border: 5px solid black;",10,
+      collapsibleTreeOutput("jobtree",height="600px") ),
+       column(1) ),
+  br(),
+  hr(),
    # Sidebar with a slider input for number of bins 
    fluidRow(
     
      column(4,
        wellPanel(
        sliderInput("branchL","Branch Length:", ticks=F,
-                     min = 100,max = 800,value = 350,)
+                     min = 100,max = 800,value = 350)
     
      
    )
-   )),
-  br(),
-      # Show a plot of the generated distribution
-     
-      collapsibleTreeOutput("jobtree")
-      
-      
+   ))
 )
    
 
@@ -43,7 +48,21 @@ ui <- fluidPage(
 server <- function(input, output) {
    
    output$jobtree <- renderCollapsibleTree({
-     collapsibleTree(jobs[-1,],hierarchy=c("Level1","Level2","Level3","Level4"),zoomable=T,collapsed=T,nodeSize = "leafCount",linkLength = input$branchL,tooltip=T,root="The Jobverse",fill = jobs$jobcol[-1],fillByLevel = F)})
+     collapsibleTree(jobs[-1,],hierarchy=c("Level1","Level2","Level3","Level4"),zoomable=T,collapsed=T,nodeSize = "leafCount",linkLength = input$branchL,tooltip=T,root="The Jobverse",fill = jobs$jobcol[-1],fillByLevel = F, inputId = "node")})
+   
+   output$tab<-renderTable({
+     if(length(input$node[[1]])!=0)
+     {
+     selectedjob<-input$node[[1]]
+     desiredcols<-c("ttl","Employment.2016","Employment.2026","ChgEmploy.2016to26.Num","ChgEmploy.2016to26.Perc","Occupational.openings..2016.26.annual.average","Median.annual.wage..2017.1.","Typical.education.needed.for.entry","Work.experience.in.a.related.occupation","Typical.on.the.job.training.needed.to.attain.competency.in.the.occupation")
+     selectedrow<-jobs[which(jobs$ttl==selectedjob),desiredcols]
+     
+     names(selectedrow)<-c("Job Title","# Employed 2016","Exp # Employed 2026","Exp # Change in Eployment 2016-2026","Exp % Change in Employment 2016-2026","Exp Job Openings 2016-2026","Median Yearly Wages 2017", "Typ Ed Needed","Work Experience Needed", "Typ On-the-Job Training Needed")
+     selectedrow
+     }else{"Nothing Selected"}
+   })
+   
+     observe(output$str <- renderPrint(str(input$node)))
    
 }
 
